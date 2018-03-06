d68a2eb3792ddcc328599a772535edeaf18cc3e8
yuhwan <yuhwan@delightroom.com>
2018-03-06 10:02:52 +0900

    Change tests for a new report structure

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -1,60 +1,69 @@
 'use strict';
 
 const test = require('ava').test;
-const SlackReport = require('../src/ReportBuilder');
+const ReportBuilder = require('../src/ReportBuilder');
 const basicAtt = require('./attachments/basic.json');
-const singleColumnAtt = require('./attachments/single-column.json');
 const fullAtt = require('./attachments/full.json');
 
-const Color = SlackReport.Color;
-
-const dau = [['Total', '10.2m', '(+2.0%)'], ['Android', '8.12m', '(+1.8%)'], ['iOS', '2.13m', '(+2.2%)']];
-const mau = [['Total', '50.3m', '(+1.2%)'], ['Android', '38.2m', '(+0.8%)'], ['iOS', '12.1m', '(+0.4%)']];
-const newUsers = [['Total', '21.2k', '(+4.2%)'], ['Android', '10.1k', '(+3.8%)'], ['iOS', '11.1k', '(+1.4%)']];
-const download = [['Total', '502m', '(+1.2%)'], ['Android', '351m', '(+0.8%)'], ['iOS', '150m', '(+0.4%)']];
-
-test('throws an error when slack url is not valid', t => {
-	const report = new SlackReport()
-		.title('title')
-		.metric('dau', [['1', '2']])
-		.build();
-	const error = t.throws(() => {
-		report.publish('http://www.google.com');
-	});
-	t.is(error.message, `The slack webhook url should starts with 'https://hooks.slack.com/services/'`);
-});
+const dau = [
+	['Total', '100.2m', '⬆︎ +2.0%'],
+	['Android', '80.12m', '⬆︎ +1.8%'],
+	['iOS', '20.13m', '⬆︎ +2.2%']
+];
+const mau = [
+	['Total', '50.3m', '⬆︎ +1.2%'],
+	['Android', '38.2m', '⬆︎ +0.8%'],
+	['iOS', '12.1m', '⬆︎ +0.4%']
+];
+const description = 'This is a sample report made by Slack Report.';
 
 test('returns basic attachement object', t => {
-	const report = new SlackReport()
+	const reportBuilder = new ReportBuilder()
 		.title('Daily User Report')
-		.metric('DAU', dau)
-		.build();
-	t.deepEqual(report._attachments, basicAtt);
+		.titleLink('https://title.link')
+		.description(description);
+	// Set DAU metric
+	const dauOption = {
+		title: 'DAU',
+		description,
+		color: '#36a64f',
+		author: 'Zeppelin',
+		authorIcon: 'https://author_icon.png',
+		timestamp: 1234567890000
+	};
+	reportBuilder.metric(dau, dauOption);
+	// Build a new report
+	const report = reportBuilder.build();
+	t.deepEqual(report._message, basicAtt);
 });
 
-test('returns single-column attachement object', t => {
-	const report = new SlackReport()
+test('returns full attachement object', async t => {
+	// Instantiate a new ReportBuilder
+	const reportBuilder = new ReportBuilder()
 		.title('Daily User Report')
-		.metric('DAU', dau)
-		.metric('MAU', mau)
-		.metric('NEW', newUsers, false)
-		.build();
-	t.deepEqual(report._attachments, singleColumnAtt);
-});
-
-test('returns full attachement object', t => {
-	const report = new SlackReport()
-		.color(Color.GOOD)
-		.icon('https://author.icon.png')
-		.category('Alarmy')
-		.title('Daily User Report', 'https://title.link')
-		.metric('DAU', dau)
-		.metric('MAU', mau)
-		.metric('NEW', newUsers)
-		.metric('DWNLD', download)
-		.description('Description: this is a sample report made by Slack Report.')
-		.publisher('Zeppelin', 'https://footer.icon.png')
-		.publishTime(1500000000000)
-		.build();
-	t.deepEqual(report._attachments, fullAtt);
+		.titleLink('https://title.link')
+		.description(description);
+	// Set DAU metric
+	const dauOption = {
+		title: 'DAU',
+		description,
+		color: '#36a64f',
+		author: 'Zeppelin',
+		authorIcon: 'https://author_icon.png',
+		timestamp: 1234567890000
+	};
+	reportBuilder.metric(dau, dauOption);
+	// Set MAU metric
+	const mauOption = {
+		title: 'MAU',
+		description,
+		color: '#36a64f',
+		author: 'Zeppelin',
+		authorIcon: 'https://author_icon.png',
+		timestamp: 1234567890000
+	};
+	reportBuilder.metric(mau, mauOption);
+	// Build a new report
+	const report = reportBuilder.build();
+	t.deepEqual(report._message, fullAtt);
 });
--- test/report-builder.test.js
+++ test/report-builder.test.js
@@ -7,189 +7,39 @@ const {formatString} = require('../src/util');
 
 const Color = ReportBuilder.Color;
 
-test('sets color to color variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.color(Color.GOOD);
-	t.is(reportBuilder._color, Color.GOOD);
-});
-
-test('throws an error when the parameter of color is not a string', t => {
-	const error = t.throws(() => {
-		const reportBuilder = new ReportBuilder() /* eslint no-unused-vars: "off" */
-			.color(100);
-	});
-	t.is(error.message, 'color only allows string parameter.');
-});
-
-test('throws an error when the parameter of color is not hex color', t => {
-	const error = t.throws(() => {
-		const reportBuilder = new ReportBuilder() /* eslint no-unused-vars: "off" */
-			.color('this is not hex');
-	});
-	t.is(error.message, `color only allows hex code color or the option provided by ReportBuilder.Color.`);
-});
-
-test('sets url to url variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.icon('https://report-icon.com');
-	t.is(reportBuilder._icon, 'https://report-icon.com');
-});
-
-test('throws an error when the parameter of icon does not start with http', t => {
-	const error = t.throws(() => {
-		const reportBuilder = new ReportBuilder()
-			.icon('www.google.com');
-	});
-	t.is(error.message, 'icon only allows url which starts with http or https.');
-});
-
-test('sets category to category variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.category('general');
-	t.is(reportBuilder._category, 'general');
-});
-
-test('casts non-string parameter of category to a string and stores it to category variable.', t => {
-	const reportBuilder = new ReportBuilder()
-		.category(1);
-	t.is(reportBuilder._category, '1');
-});
-
-test('sets title to title variable', t => {
+test('sets title to _title variable', t => {
 	const reportBuilder = new ReportBuilder()
 		.title('report');
 	t.is(reportBuilder._title, 'report');
 });
 
-test('casts non-string title parameter to a string and stroes it to title varaible', t => {
+test('casts non-string title parameter to a string and stroes it to _title varaible', t => {
 	const reportBuilder = new ReportBuilder()
 		.title(1);
 	t.is(reportBuilder._title, '1');
 });
 
-test('sets titleLink parameter to titleLink variable', t => {
+test('sets titleLink parameter to _titleLink variable', t => {
 	const reportBuilder = new ReportBuilder()
-		.title('report', 'https://title-link.com');
+		.titleLink('https://title-link.com');
 	t.is(reportBuilder._titleLink, 'https://title-link.com');
 });
 
 test('throws an error when the titleLink is not a url string', t => {
 	const error = t.throws(() => {
 		const reportBuilder = new ReportBuilder()
-			.title(1, 'www.google.com');
+			.titleLink('www.google.com');
 	});
 	t.is(error.message, 'titleLink should be a url string.');
 });
 
-test('sets description parameter with code markdown to description variable', t => {
+test('sets description parameter to _titleLink variable', t => {
 	const reportBuilder = new ReportBuilder()
 		.description('description');
-	t.is(reportBuilder._description, '```description```');
+	t.is(reportBuilder._description, 'description');
 });
 
-test('casts non-string description parameter to string and stores it into description variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.description(101);
-	t.is(reportBuilder._description, '```101```');
-});
-
-test('set publisher parameter to publisher variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.publisher('publisher');
-	t.is(reportBuilder._publisher, 'publisher');
-});
-
-test('casts non-string publisher parameter to string and stores it into publisher variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.publisher(101);
-	t.is(reportBuilder._publisher, '101');
-});
-
-test('sets publisherLink parameter to publisherLink variable', t => {
-	const reportBuilder = new ReportBuilder()
-		.publisher(101, 'https://publisher-link.com');
-	t.is(reportBuilder._publisherLink, 'https://publisher-link.com');
-});
-
-test('throws an error when the publisherLink is not a url string', t => {
-	const error = t.throws(() => {
-		const reportBuilder = new ReportBuilder()
-			.publisher(101, 'publisher-link.com');
-	});
-	t.is(error.message, 'publisherLink should be a url string.');
-});
-
-test('casts publishTime as unix timestamp and stores it to publishTime variable', t => {
-	const now = Date.now();
-	const reportBuilder = new ReportBuilder()
-		.publishTime(now);
-	t.is(reportBuilder._publishTime, Math.floor(now / 1000));
-});
-
-test('throws an error when the passed publishTime is not a timestamp', t => {
-	const error = t.throws(() => {
-		const reportBuilder = new ReportBuilder()
-			.publishTime('today');
-	});
-	t.is(error.message, 'publishTime should be a 13-digit timestamp.');
-});
-
-test('stores the name and data passed by metric to metrics array variable', t => {
-	const name = 'Today';
-	const data = [
-		['DAU', '200', '(+2.0%)'],
-		['MAU', '1000', '(+4.0%)']
-	];
-	const reportBuilder = new ReportBuilder()
-		.metric(name, data);
-	t.deepEqual(reportBuilder._metrics, [{
-		title: name,
-		value: formatString(data),
-		short: true
-	}]);
-});
-
-test('stores the name, data and short passed by metric to metrics array variable', t => {
-	const name = 'Today';
-	const data = [
-		['DAU', '200', '(+2.0%)'],
-		['MAU', '1000', '(+4.0%)']
-	];
-	const reportBuilder = new ReportBuilder()
-		.metric(name, data, false);
-	t.deepEqual(reportBuilder._metrics, [{
-		title: name,
-		value: formatString(data),
-		short: false
-	}]);
-});
-
-test('pushs the name and data passed by second metric call to metrics array', t => {
-	const name = 'Today';
-	const data = [
-		['DAU', '200', '(+2.0%)'],
-		['MAU', '1000', '(+4.0%)']
-	];
-	const reportBuilder = new ReportBuilder()
-		.metric(name, data)
-		.metric(name, data);
-	t.deepEqual(reportBuilder._metrics, [
-		{title: name, value: formatString(data), short: true},
-		{title: name, value: formatString(data), short: true}
-	]);
-});
-
-test('throws an error when the short parameter is not a boolean type', t => {
-	const name = 'Today';
-	const data = [['DAU', '200', '(+2.0%)']];
-	const error = t.throws(() => {
-		const reportBuilder = new ReportBuilder()
-			.metric(name, data, 'long');
-	});
-	t.is(error.message, 'The short parameter should be a boolean type.');
-});
-
-test('throws an error when invoking build without title', t => {
+test('throws an error when invoking build function before setting title', t => {
 	const error = t.throws(() => {
 		const report = new ReportBuilder() /* eslint no-unused-vars: "off" */
 			.build();
@@ -197,7 +47,7 @@ test('throws an error when invoking build without title', t => {
 	t.is(error.message, 'The title should be defined.');
 });
 
-test('throws an error when invoking build withouth metrics', t => {
+test('throws an error when invoking build function before setting metrics', t => {
 	const error = t.throws(() => {
 		const report = new ReportBuilder() /* eslint no-unused-vars: "off" */
 			.title('test')
@@ -209,7 +59,49 @@ test('throws an error when invoking build withouth metrics', t => {
 test('returns an instance of Report', t => {
 	const report = new ReportBuilder()
 		.title('title')
-		.metric('name', [['1', '2', '3']])
+		.metric([['1', '2', '3']])
 		.build();
 	t.is(report instanceof Report, true);
+});
+
+test('add data to _metrics array as a data field of an object', t => {
+	const reportBuilder = new ReportBuilder()
+		.metric([['1', '2', '3']]);
+	t.is(Array.isArray(reportBuilder._metrics), true);
+	t.deepEqual(reportBuilder._metrics[0].data, [['1', '2', '3']]);
+});
+
+test('throws an error when data parameter is not passed', t => {
+	const error = t.throws(() => {
+		(new ReportBuilder()).metric();
+	});
+	t.is(error.message, 'You should pass data to metric function.');
+});
+
+test('throws an error when data is not a two dimensional array', t => {
+	const error = t.throws(() => {
+		(new ReportBuilder()).metric(['one dimensioanl array']);
+	});
+	t.is(error.message, 'data parameter for metric should be a two dimensional array.');
+});
+
+test('throws an error when authorIcon option does not starts with http', t => {
+	const error = t.throws(() => {
+		(new ReportBuilder()).metric([['1']], {authorIcon: 'icon'});
+	});
+	t.is(error.message, 'authorIcon only allows url which starts with http or https.');
+});
+
+test('throws an error when timestamp option is not a 13-digits number', t => {
+	const error = t.throws(() => {
+		(new ReportBuilder()).metric([['1']], {timestamp: 'abc'});
+	});
+	t.is(error.message, 'timestamp should be a 13-digits.');
+});
+
+test('throws an error when color option is not hex code', t => {
+	const error = t.throws(() => {
+		(new ReportBuilder()).metric([['1']], {color: 'abc'});
+	});
+	t.is(error.message, 'color only allows hex code color or the option provided by ReportBuilder.Color.');
 });
